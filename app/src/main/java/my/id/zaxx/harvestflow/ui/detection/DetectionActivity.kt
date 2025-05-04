f106e346a944c0bd76204eacae123226afc6e129
Dewantara <drawww111@gmail.com>
2025-05-05 01:53:53 +0900

    Feat (Detection) : Implement Detection UI

--- app/src/main/java/my/id/zaxx/harvestflow/ui/detection/DetectionActivity.kt
+++ app/src/main/java/my/id/zaxx/harvestflow/ui/detection/DetectionActivity.kt
@@ -1,21 +1,51 @@
 package my.id.zaxx.harvestflow.ui.detection
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
 import my.id.zaxx.harvestflow.R
+import my.id.zaxx.harvestflow.adapter.DetectionPagesAdapter
+import my.id.zaxx.harvestflow.databinding.ActivityDetectionBinding
+import my.id.zaxx.harvestflow.ui.detection.manulinputsensor.ManualInputFragment
+import my.id.zaxx.harvestflow.ui.detection.sensorinput.SensorInputFragment
 
 class DetectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetectionBinding
+    private lateinit var viewPagerAdapter : DetectionPagesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detection)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityDetectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViewPager()
+
+    }
+
+    private fun setupViewPager(){
+        val listOfFragment = listOf(
+            SensorInputFragment(),
+            ManualInputFragment()
+        )
+
+        viewPagerAdapter = DetectionPagesAdapter(
+            listOfFragment,
+            supportFragmentManager,
+            lifecycle
+        )
+
+        binding.viewPager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when ( position) {
+                0 -> "Sensor Input"
+                1 -> "Manual Input"
+                else -> ""
+            }
+        }.attach()
     }
 }
