6ba7d8a6af9fa6a300ace72714378b1e5e449a13
Dewantara <drawww111@gmail.com>
2025-06-26 06:26:50 +0900

    Add Play Services Location dependency
    Uncommented and added the Play Services Location library to dependencies. Also removed commented-out Retrofit and Gson dependencies for cleaner configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,13 +78,13 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-//    implementation ("com.squareup.retrofit2:converter-gson:2.12.1")
-//    implementation ("com.google.code.gson:gson:2.12.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
 
     implementation ("com.microsoft.onnxruntime:onnxruntime-android:latest.release")
 
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    implementation (libs.play.services.location)
 
 }
