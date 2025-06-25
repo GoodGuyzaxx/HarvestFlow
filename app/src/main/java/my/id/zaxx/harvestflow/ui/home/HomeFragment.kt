252437eab1eed2639b6c89a31c2abcd0c245e3f9
Dewantara <drawww111@gmail.com>
2025-06-26 06:30:14 +0900

    Add location and notification permissions, refactor sensors
    Integrated location and notification permission requests, created notification channel for Android 8+, and refactored sensor data retrieval methods. Removed unused rain sensor and relay code, and improved weather status fetching using device location.

--- app/src/main/java/my/id/zaxx/harvestflow/ui/home/HomeFragment.kt
+++ app/src/main/java/my/id/zaxx/harvestflow/ui/home/HomeFragment.kt
@@ -1,13 +1,24 @@
 package my.id.zaxx.harvestflow.ui.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -16,8 +27,6 @@ import com.google.firebase.database.ValueEventListener
 import dagger.hilt.android.AndroidEntryPoint
 import my.id.zaxx.harvestflow.R
 import my.id.zaxx.harvestflow.data.model.LightSensor
-import my.id.zaxx.harvestflow.data.model.RainSensor
-import my.id.zaxx.harvestflow.data.model.RelayPower
 import my.id.zaxx.harvestflow.data.model.SoilSensor
 import my.id.zaxx.harvestflow.data.model.TempSensor
 import my.id.zaxx.harvestflow.databinding.FragmentHomeBinding
@@ -32,10 +41,10 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var firebase : FirebaseDatabase
     private lateinit var database: DatabaseReference
-    private lateinit var power : RelayPower
-    private var readPowerValue : Boolean = false
     private var valueEventListener: ValueEventListener? = null
     private val viewModel : HomeViewModel by viewModels()
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,42 +52,31 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container ,false)
-//        buttonTheme()
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+
+        /*Function To Run*/
+        getNotifChannel()
+        requestAllPermissions()
         getTempSensor()
         getSoilSensor()
-        getRainSensor()
         getLuxSensor()
-//        getRelay()
-//        binding.btnPump.setOnClickListener {
-//            buttonTheme()
-//            if (readPowerValue == true){
-//                sendRelay(false)
-//            } else {
-//                sendRelay(true)
-//            }
-//        }
+        getWeatherStatus()
 
-
-        val lat = "-2.53371"
-        val lon = "140.71813"
 
         binding.tvDateTime.text = formatDate()
-
         viewModel.errorResponse.observe(viewLifecycleOwner){
             Toast.makeText(requireContext(), it , Toast.LENGTH_SHORT).show()
         }
 
-        viewModel.getWeather(lat,lon)
         viewModel.weatherResponse.observe(viewLifecycleOwner){
             val valueCurrentTemp = convertKelvinToCelcius(it.main.temp.toString().toDouble())
-
-
             binding.tvCurrentTemp.text = resources.getString(R.string.room_temp, valueCurrentTemp)
             binding.tvLocation.text = it.name.toString()
             binding.tvWindSpeed.text = resources.getString(R.string.wind_speed, it.wind.speed)
@@ -87,19 +85,19 @@ class HomeFragment : Fragment() {
         }
     }
 
-
+    /*Get Sensor Value From Database*/
     private fun getTempSensor() {
         firebase = FirebaseDatabase.getInstance()
         database = firebase.getReference("temp_sensor")
-
-        // Create the listener
         valueEventListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 if (_binding == null) return
                 val tempSensor = snapshot.getValue(TempSensor::class.java)
                 tempSensor?.let {
-                    binding.tvTemperature.text = resources.getString(R.string.room_temp,it.celcius )
-                    binding.tvEnvHumidity.text = resources.getString(R.string.env_humi, it.humd)
+                    val valueCel = it.celcius.toString()
+                    val valueHumd = it.humd.toString()
+                    binding.tvTemperature.text = resources.getString(R.string.room_temp,valueCel )
+                    binding.tvEnvHumidity.text = resources.getString(R.string.env_humi, valueHumd)
                 }
             }
 
@@ -112,6 +110,7 @@ class HomeFragment : Fragment() {
         database.addValueEventListener(valueEventListener!!)
     }
 
+    /*Get Sensor Value From Database*/
     private fun getSoilSensor(){
         firebase = FirebaseDatabase.getInstance()
         database = firebase.getReference("soil_sensor")
@@ -121,8 +120,8 @@ class HomeFragment : Fragment() {
                 if (_binding == null) return
                 val valueSoil = snapshot.getValue(SoilSensor::class.java)
                 valueSoil?.let {
-                    binding.tvHumidity.text = resources.getString(R.string.soil_humi, it.value)
-                    binding.progressSoilHumidity.setProgress(it.value.toInt())
+                    binding.tvHumidity.text = resources.getString(R.string.soil_humi, it.value.toString())
+                    binding.progressSoilHumidity.setProgress(it.value)
                 }
             }
 
@@ -134,27 +133,7 @@ class HomeFragment : Fragment() {
         database.addValueEventListener(valueEventListener!!)
     }
 
-    private fun getRainSensor(){
-        firebase = FirebaseDatabase.getInstance()
-        database = firebase.getReference("water_sensor")
-
-        valueEventListener = object  : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (_binding == null) return
-                val rainSensorValue = snapshot.getValue(RainSensor::class.java)
-                rainSensorValue?.let {
-                    binding.tvRainStatus.text = it.status
-                    Log.d("TAG", "onDataChangeRain: ${it.status}")
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(activity, "Failed to retrieve data: ${error.message}", Toast.LENGTH_SHORT).show()
-            }
-        }
-        database.addValueEventListener(valueEventListener!!)
-    }
-
+    /*Get Sensor Value From Database*/
     private fun getLuxSensor(){
         firebase = FirebaseDatabase.getInstance()
         database = firebase.getReference("light_sensor")
@@ -175,63 +154,81 @@ class HomeFragment : Fragment() {
         database.addValueEventListener(valueEventListener!!)
     }
 
+    /*To Make Notification Channel For Android 8 and Higher*/
+    private fun getNotifChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                DESCRIPTION,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableLights(true)
+                lightColor = Color.GREEN
+                enableVibration(true)
+            }
+            val notificationManager = requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    /*Request For ASK 2 Permission*/
+    private fun requestAllPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissionsToRequest.toTypedArray(),
+                100
+            )
+        }
+    }
+
+    /*Get Longitude and Latitude */
+    @SuppressLint("MissingPermission")
+    private fun getWeatherStatus(){
+        val location = fusedLocationProviderClient.lastLocation
+        location.addOnSuccessListener {
+            if (it!=null){
+                val textLat = it.latitude.toString()
+                val textLon = it.longitude.toString()
+                Log.d("TAG", "getWeatherStatus: $textLon + $textLat")
+                viewModel.getWeather(textLat,textLon)
+
+            }
+        }
+    }
+
+    /*For Convert Kelvin To Celcius In Api Weather*/
     private fun convertKelvinToCelcius(value : Double): Any {
         val kMinus = -273.15
         val cValue = value + kMinus
         return  cValue.roundToInt()
     }
 
+    /*For Change Format Date*/
     private fun formatDate(): String {
-        // Create formatter with the desired pattern
         val dateFormat = SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault())
-
-        // You can use current date or a specific date
-        val currentDate = Date() // For current date
-
-        // For a specific date (e.g., March 22, 2025)
-        // val specificDate = Calendar.getInstance()
-        // specificDate.set(2025, Calendar.MARCH, 22)
-        // return dateFormat.format(specificDate.time)
-
+        val currentDate = Date()
         return dateFormat.format(currentDate)
     }
-
-//    private fun getRelay(){
-//        firebase = FirebaseDatabase.getInstance()
-//        database = firebase.getReference("relay_module")
-//
-//        valueEventListener = object  : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                if (_binding == null ) return
-//                val relayPowerValue = snapshot.getValue(RelayPower::class.java)
-//                relayPowerValue?.let {
-//                    readPowerValue = it.power
-//                    Log.d("TAG", "onDataChangeRelay: ${it.power}")
-//                }
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                Toast.makeText(activity, "Failed to retrieve data: ${error.message}", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//        database.addValueEventListener(valueEventListener!!)
-//    }
-
-//    private fun sendRelay(value : Boolean){
-//        firebase =FirebaseDatabase.getInstance()
-//        database = firebase.getReference("relay_module")
-//        database.child("power")
-//        power = RelayPower()
-//        power.power = value
-//
-//        database.setValue(power)
-//            .addOnSuccessListener {
-//                Toast.makeText(activity, "berhasil", Toast.LENGTH_SHORT).show()
-//            }
-//            .addOnFailureListener{
-//                Toast.makeText(activity, "gagal ${it.message}", Toast.LENGTH_SHORT).show()
-//            }
-//    }
 
 
     override fun onDestroyView() {
@@ -245,15 +242,11 @@ class HomeFragment : Fragment() {
         _binding = null
         super.onDestroyView()
     }
-//
-//    override fun onDestroy() {
-//
-//        if (::database.isInitialized && ::valueEventListener.isInitialized) {
-//            database.removeEventListener(valueEventListener)
-//        }
-//
-//        _binding = null
-//
-//        super.onDestroyView()
-//    }
+
+    companion object {
+        const val CHANNEL_ID = "zaxx"
+        const val NOTIFICATION_ID = 0
+        const val DESCRIPTION = "NOTIF CHANNEL"
+    }
+
 }
