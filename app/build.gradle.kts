2d538e829602519f769a3dc48e09a7a5795e4ed8
Dewantara <drawww111@gmail.com>
2025-07-04 23:11:19 +0900

    Update dependencies and version name in build files
    Changed app versionName to '1.0.0-alpah' and updated dependencies in build.gradle.kts: commented out libs.firebase.database, added explicit Firebase Database and Retrofit dependencies, and reorganized dependency declarations. Removed unused TensorFlow Lite versions from libs.versions.toml.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "1.0.0-alpah"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -69,7 +69,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.firebase.database)
+//    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -77,14 +77,19 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-
     implementation ("com.microsoft.onnxruntime:onnxruntime-android:latest.release")
 
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     implementation (libs.play.services.location)
+
+    // Firebase Realtime Database untuk monitoring
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
+    // Retrofit untuk komunikasi HTTP ke server Flask
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+
 
 }
